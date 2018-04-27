25be1ab3c293235f5e9ac7545113e3234d5b9e7e
zhang4838223 <409751089@qq.com>
2018-04-27 23:33:50 +0800

    修改消费端获取配置中心配置项

--- consumerserver/src/main/java/com/zxj/spring/controller/IndexController.java
+++ consumerserver/src/main/java/com/zxj/spring/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.zxj.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,14 @@ public class IndexController {
 
     @Autowired
     private RestTemplate restTemplate;
+    //这个@Value会根据配置的配置中心地址找到git仓库对应的配置和本地服务的配置文件
+    @Value("${path}")
+    private String path;
 
     @GetMapping("/index/")
     public String index(){
         ResponseEntity<String> forEntity = restTemplate.getForEntity("http://provider-server/user/list", String.class);
         System.out.println(forEntity);
-        return "zhangxj";
+        return "zhangxj:" + path;
     }
 }
